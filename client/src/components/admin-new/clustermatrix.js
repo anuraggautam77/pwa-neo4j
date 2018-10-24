ef35344b992319c0bb3df0192f23f82de35da700
Anurag <Anuraggauutam77@gmail.com>
2018-10-24 18:20:19 +0530

    add loader

--- client/src/components/admin-new/clustermatrix.js
+++ client/src/components/admin-new/clustermatrix.js
@@ -1,25 +1,27 @@
 import React, { Component } from "react";
+
 class ClusterMatrix extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            matrix: props.matrix,
-            centriod: props.centriod
+            matrix: props.matrix
+           
         };
     }
     componentWillReceiveProps(props) {
-        this.setState({matrix: props.matrix, centriod: props.centriod});
+        this.setState({matrix: props.matrix});
     }
     
     rowcol() {
        
         var template = this.state.matrix.map((obj, i) => {
+            console.log(obj)
             return (
                     <div className="row" style={{"borderBottom":"1px solid #2f455a"}} key={i}   >
-                        <div className="col-md-2">{obj.cid + 1}</div>
-                        <div className="col-md-3">{obj.count}</div>
-                        <div className="col-md-3">{(obj.distance.reduce((x,y) => x + y, 0)/obj.distance.length).toFixed(2)} </div>
-                        <div className="col-md-4">{Math.max(...obj.distance).toFixed(2)}</div>
+                        <div className="col-md-2">{i + 1}</div>
+                        <div className="col-md-3"></div>
+                        <div className="col-md-3">{(obj.mean).toFixed(2)} </div>
+                        <div className="col-md-4">{(obj.amax).toFixed(2)}</div>
                     </div>
                         );
 
@@ -27,7 +29,7 @@ class ClusterMatrix extends Component {
         return template;
     }
     render() {
-        console.log(this.state.matrix);
+      
         return (
                 <div className="row zipcode-filter">
                     <div className="well panel-heading alignheading">
@@ -40,7 +42,7 @@ class ClusterMatrix extends Component {
                                 <div className="row" style={{"borderBottom":"1px solid #2f455a"}}>
                                     <div className="col-md-2 label-white"><h6>S.no</h6></div>
                                     <div className="col-md-3 label-white"><h6>Users</h6></div>
-                                    <div className="col-md-3 label-white"><h6>Min Dis</h6></div>
+                                    <div className="col-md-3 label-white"><h6>Avg Dis</h6></div>
                                     <div className="col-md-4 label-white"><h6>Max Dis</h6></div>
                                 </div>
                                 {this.rowcol()}
--- client/src/containers/adminnew.js
+++ client/src/containers/adminnew.js
@@ -1,17 +1,35 @@
 import React, { Component } from "react";
-import '../style/css/admin.scss';
-import Adminpanel from "../components/admin-new/index";
-export default class AdminDashboard extends Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
+        import '../style/css/admin.scss';
+        import Adminpanel from "../components/admin-new/index";
+        import Loader from 'react-loader-spinner';
+        export default class AdminDashboard extends Component {
+        constructor(props) {
+            super(props);
+            this.state={
+                loaderShow:"dn"
+            }
+        }
+        
+        
+        
+        render() {
         return (
-                <div>
-                    <div className="adminpanel main-landing row">
-                        <Adminpanel />
-                    </div>
-                </div>
-                            );
-                }
+<div>
+    <div className="adminpanel main-landing row">
+        <div id="main">
+            <div id="header">
+                <h4> <span className="glyphicon glyphicon-globe" aria-hidden="true"  /> 
+                    MRU APP
+                </h4>
+            </div>
+            <div className={`modal ${this.state.loaderShow}`} style={{ "paddingLeft": "40%", "paddingTop":"28%" }}>
+                 <Loader type="Puff"  color="#ff2266fc" height="75" width="75"  /> 
+            </div>
+           
+            <Adminpanel  updateLoader={(value)=>{ this.setState({"loaderShow":value})}} />
+        </div>
+        </div>
+    </div>
+    )
+    }
     }
