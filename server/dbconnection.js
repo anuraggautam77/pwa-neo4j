0bccb73cbaf2a66a0567983985a5f8ba9c6fb153
Anurag <Anuraggauutam77@gmail.com>
2018-10-31 12:55:37 +0530

    console.log process

--- server/dbconnection.js
+++ server/dbconnection.js
@@ -7,12 +7,17 @@ const dbConfig = {url: '35.198.237.23:4502/', username: 'neo4j', pass: 'neo4j123
  username:  http://35.198.237.23:4502/browser/
  password: neo4j123
  bolt url: bolt://35.196.83.113:8080
-at https://35.196.83.113:4503/browser/
+ at https://35.196.83.113:4503/browser/
  */
 
 
 
 ConnectionEstablish = function () {
+
+    console.log(">>>>>>>>>>>>>>>>>>>>");
+    console.log(process.env)
+
+
     var dbConnection = null;
     dbConnection = new neo4j.GraphDatabase(`${dbConfig.host}//${dbConfig.username}:${dbConfig.pass}@${dbConfig.url}`);
     var getInstance = function () {
