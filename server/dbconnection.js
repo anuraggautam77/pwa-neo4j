456c50ac59600b514c7a9d39fd07b93597ebce9d
Anurag <Anuraggauutam77@gmail.com>
2018-10-31 13:00:17 +0530

    Add process credential

--- server/dbconnection.js
+++ server/dbconnection.js
@@ -1,17 +1,6 @@
 const neo4j = require('neo4j');
 //const  dbConfig = {url: 'localhost:7474/', username: 'neo4j', pass: 'demo', host: 'http:'};
-const dbConfig = {url: '35.198.237.23:4502/', username: 'neo4j', pass: 'neo4j123', host: 'http:'};
-
-
-/*
- username:  http://35.198.237.23:4502/browser/
- password: neo4j123
- bolt url: bolt://35.196.83.113:8080
- at https://35.196.83.113:4503/browser/
- */
-
-
-
+ const dbConfig = {url:process.env.URL+'/', username: process.env.USER_NAME, pass:  process.env.PASSWORD, host: 'http:'};
 ConnectionEstablish = function () {
 
     console.log(">>>>>>>>>>>>>>>>>>>>");
